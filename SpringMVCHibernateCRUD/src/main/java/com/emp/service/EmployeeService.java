437fe605b36dd5e97e853bebe822e9e379d6e6e6
mohandupampa <46498671+mohandupampa@users.noreply.github.com>
2019-01-10 23:57:58 +0530

    Add files via upload

---
+++ SpringMVCHibernateCRUD/src/main/java/com/emp/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.emp.service;
+
+import java.util.List;
+
+import com.emp.model.Employee;
+
+public interface EmployeeService {
+	
+	public void addEmployee(Employee employee);
+
+	public List<Employee> getAllEmployees();
+
+	public void deleteEmployee(Integer employeeId);
+
+	public Employee getEmployee(int employeeid);
+
+	public Employee updateEmployee(Employee employee);
+}
